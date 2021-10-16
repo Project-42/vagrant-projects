d61cdc784b48f36741437da9b50cc7d193ac882a
solifugo <solifugo@server.localdomain>
2021-10-16 18:55:20 +0100

    a lot of changes

--- OracleRAC/bash-it_files/custom.bash
+++ OracleRAC/bash-it_files/custom.bash
@@ -3,9 +3,5 @@ export HISTFILESIZE=300000
 export HISTSIZE=300000
 export HISTTIMEFORMAT="%h/%d - %H:%M:%S "
 
-## KVM ##
-export LIBVIRT_DEFAULT_URI=qemu:///system
-export VIRSH_DEFAULT_CONNECT_URI=qemu:///system
-
 ## Add to PATH ##
 PATH=~/bin:~/.local/bin:$PATH
--- OracleRAC/bash-it_files/custom.completion.bash
+++ OracleRAC/bash-it_files/custom.completion.bash
@@ -4,17 +4,6 @@ hi (){
 history |grep -i $1
 }
 
-start (){
-virsh start $1
-}
-
-stop (){
-virsh shutdown $1
-}
-
-destroy (){
-virsh destroy $1
-}
 
 ali (){
 alias |grep -i $1

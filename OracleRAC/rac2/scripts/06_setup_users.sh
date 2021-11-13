de820fd390032db6cd618b39b1c2390153eb52d0
solifugo <solifugo@server.localdomain>
2021-11-13 16:12:19 +0000

    a lot of changes

--- OracleRAC/rac2/scripts/06_setup_users.sh
+++ OracleRAC/rac2/scripts/06_setup_users.sh
@@ -175,8 +175,8 @@ chmod +x -R /home/oracle/bin/* /home/grid/bin/*
 echo "-----------------------------------------------------------------"
 echo -e "${INFO}`date +%F' '%T`: Installing sqlcl using Connor's Script "
 echo "-----------------------------------------------------------------"
-su -l oracle -c "wget -O /home/oracle/bin/getsqlcl.sh https://github.com/connormcd/misc-scripts/blob/master/getsqlcl.sh"
-su -l oracle -c "chmod +x /home/oracle/bin/getsqlcl.sh"
+su -l oracle -c "git clone https://github.com/connormcd/misc-scripts.git /home/oracle/bin"
+su -l oracle -c "chmod +x -R /home/oracle/bin/*"
 su -l oracle -c "/home/oracle/bin/getsqlcl.sh"
 
 

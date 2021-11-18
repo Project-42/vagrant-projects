001c5f581c1bb98bd0fc4c419effee5c85f097cc
solifugo <solifugo@server.localdomain>
2021-11-18 16:55:43 +0000

    bash funcions added

--- OracleRAC/rac1/bash-it_files/custom.completion.bash
+++ OracleRAC/rac1/bash-it_files/custom.completion.bash
@@ -8,3 +8,18 @@ history |grep -i $1
 ali (){
 alias |grep -i $1
 }
+
+
+dbstop (){
+srvctl stop database -d ${ORACLE_SID::-1}
+}
+
+dbstart (){
+srvctl start database -d ${ORACLE_SID::-1}
+}
+
+
+dbrestart (){
+srvctl stop database -d ${ORACLE_SID::-1}
+srvctl start database -d ${ORACLE_SID::-1}
+}
